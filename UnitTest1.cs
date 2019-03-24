206e79b1550bfddd19669c453891b595721b76ab
Stefc <stefan.boether@gmail.com>
2019-03-24 06:25:56 +0100

    First commit

---
+++ UnitTest1.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace romannumbers
+{
+    public class UnitTest1
+    {
+        [Theory]
+		[InlineData("I", 1)]
+		[InlineData("II", 2)]
+		[InlineData("IV", 4)]
+		[InlineData("V", 5)]
+		[InlineData("IX", 9)]
+		[InlineData("X", 10)]
+		[InlineData("XLII", 42)]
+		[InlineData("XCIX", 99)]
+		[InlineData("MMXIII", 2013)]
+		public void TestRomanNumber(string romanNumber, int expected)
+		{
+			Assert.Equal(expected, this.RomanToDecimal(romanNumber));
+		}
+
+        private int RomanToDecimal(string romanNumber) => 
+            romanNumber
+                .Select(RomanDigit)
+                .Aggregate( (result: 0, last: 0), (acc, digit) 
+                    => (acc.result + digit - ((digit > acc.last) ? acc.last << 1: 0), digit)).result;
+
+        private int RomanDigit(char digit) => 
+            new int[]{100, 500, 0, 0, 0, 0, 1, 1, 0, 50, 1000, 
+                0, 0, 0, 0, 0, 0, 0, 5, 5, 0, 10}[digit - 'C'];
+    }
+}
